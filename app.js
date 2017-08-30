adf9262e3247c9bc46e73f1bd656bcd154341511
Oleksiy Brain <arro.social@gmail.com>
2017-08-31 00:44:30 +0100

    Add error handling middlewares

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var path = require('path');
 var mongoose = require('mongoose');
-var apiLogger = require('./modules/logger').api;
 var http = require('http');
 var https = require('https');
 var helmet = require('helmet');
 var config = require('./env/index');
 var fs = require('fs');
+var apiLogger = require('./modules/logger').api;
 var httpLogger = require('./modules/logger').http;
 
 app.use(helmet());
@@ -27,9 +27,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 httpLogger.stream = {
-  write: function(message, encoding){
-    httpLogger.info(message);
-  }
+    write: function(message, encoding){
+        httpLogger.info(message);
+    }
 };
 app.use(morgan("combined", { stream: httpLogger.stream }));
 
@@ -43,7 +43,7 @@ app.use(express.static(path.resolve('app')));
 
 // Response to preflight requests
 app.options("/*", function(req, res, next){
-  res.send(200);
+    res.send(200);
 });
 
 app.use('/subscribers', require('./routes/subscribers'));
@@ -58,51 +58,48 @@ app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  apiLogger.error(err);
-  // logger.error(err, {url: req.originalUrl, params: req.body, customerId: req.decoded._id});
-  next(err);
+    var err = new Error({ name: 'ENOENT', message: 'page not found' });
+    err.status = 404;
+    next(err);
+});
+
+// log errors
+app.use(function(err, req, res, next) {
+    err.customer = req.decoded;
+    err.localHostname = req.hostname;
+    err.originalUrl = req.originalUrl;
+    apiLogger.error(err);
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+    app.use(function(err, req, res, next) {
+        var errorObj = { message: err.message, error: err };
+        if(err.name) errorObj.name = err.name;
+        if(err.code) errorObj.code = err.code;
 
-    //log the error
-    // err.localIp = req.ip;
-    err.localHostname = req.hostname;
-    err.originalUrl = req.originalUrl;
-    apiLogger.error(err);
-
-    // TODO: Notify developers
-
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
+        res.status(err.status || 500);
+        res.json(errorObj);
     
-  });
+    });
 }
+
+// notify developers
+app.use(function(err, req, res, next) {
+    // TODO: Notify developers
+});
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  var errorObj = { message: err.message, error: {} };
+  if(err.name) errorObj.name = err.name;
+  if(err.code) errorObj.code = err.code;
+
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-
-  // TODO: Notify developers
-
-  //log the error
-  // err.localIp = req.ip;
-  err.localHostname = req.hostname;
-  err.originalUrl = req.originalUrl;
-  apiLogger.error(err);
+  res.json(errorObj);
+  
 });
 
 //===============Start Server================
@@ -111,22 +108,13 @@ http.createServer(app).listen(config.port);
 console.log('App is listening at http port %s', config.port);
 
 if(config.ssl) {
-  options = {
-    key: fs.readFileSync(config.ssl.key),
-    cert: fs.readFileSync(config.ssl.cert)
-    // requestCert: true,
-    // rejectUnauthorized: true
-  };
+    options = {
+        key: fs.readFileSync(config.ssl.key),
+        cert: fs.readFileSync(config.ssl.cert)
+        // requestCert: true,
+        // rejectUnauthorized: true
+    };
 
-  https.createServer(options, app).listen(config.port+1);
-  console.log('App is listening at https port %s', config.port+1);
+    https.createServer(options, app).listen(config.port+1);
+    console.log('App is listening at https port %s', config.port+1);
 }
-
-// var server = app.listen(config.port, function () {
-//   console.log(server.address());
-//   var host = server.address().address;
-//   var port = server.address().port;
-
-//   apiLogger.info('App listening at http://%s:%s', host, port);
-
-// });
